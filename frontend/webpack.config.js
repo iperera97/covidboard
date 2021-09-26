8cbbf3f06437fc89562cba581255bcf1703888a7
iperera97 <isurumahesh97@gmail.com>
2021-09-26 23:05:33 +0530

    setup development environment for react

---
+++ frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+
+  entry: path.join(__dirname, "src", "index.js"),
+  
+  output: {
+    path:path.resolve(__dirname, "public"),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
+  },
+
+  devServer: {
+    static : {
+      directory : path.join(__dirname, "public/")
+    },
+    port: 3000,
+  },
+}
